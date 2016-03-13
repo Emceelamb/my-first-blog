a36d404c2418177d1f1bb0fe137291a50d4fde02
emceelamb <marktranquilizerlam@gmail.com>
2016-03-13 14:56:57 -0700

    add bootstrap and fix responsive

--- blog/models.py
+++ blog/models.py
@@ -10,6 +10,7 @@ class Post(models.Model):
             default=timezone.now)
     published_date = models.DateTimeField(
             blank=True, null=True)
+    projectimage = models.ImageField(upload_to='', height_field=None, width_field=None, max_length=100, default='https://pmcdeadline2.files.wordpress.com/2010/08/nicolas_cage.jpg')
 
     def publish(self):
         self.published_date = timezone.now()
@@ -18,17 +19,3 @@ class Post(models.Model):
     def __str__(self):
         return self.title
 
-class Project(models.Model):
-    projecttitle = models.CharField(max_length=200)
-    projectdate = models.CharField(max_length=200)
-    projectmedium = models.CharField(max_length=200)
-    projecttext = models.TextField()
-    
-    projectimage = models.ImageField(upload_to=None, height_field=None, width_field=None, max_length=100)
-    
-    def publish(self):
-        
-        self.save()
-
-    def __str__(self):
-        return self.projecttitle
--- mysite/urls.py
+++ mysite/urls.py
@@ -1,6 +1,9 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 from home import views
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     # Examples:
@@ -14,5 +17,5 @@ urlpatterns = [
     url(r'^$', ('home.views.homeImage')),
     url(r'^about/', ('home.views.about')),
     url(r'^contact/', ('home.views.contact')),
-
-]
+    
+]  + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
